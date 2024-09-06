1b0c1cf11d493a33fad682b4df1441d205cf1657
Elina <elinaviksne124@gmail.com>
2024-09-06 10:28:57 +0300

    select with logical operators

---
+++ select/cetri.sql
@@ -0,0 +1,9 @@
+use sql_store;
+-- 1. uzd
+-- select * from customers where birth_date > "1990-01-01" and points > 1000;
+
+-- 2. uzd
+-- select * from customers where birth_date > "1990-01-01"or points > 1000;
+
+-- 3. uzd
+-- select * from customers where birth_date > "1990-01-01"or points > 1000 and state in ("VA", "CA");
