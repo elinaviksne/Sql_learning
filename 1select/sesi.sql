a4bc903c3547645c9c84ee55b00099a21f36844d
Elina <elinaviksne124@gmail.com>
2024-09-12 09:39:14 +0300

    selects from one table

---
+++ 1select/sesi.sql
@@ -0,0 +1,16 @@
+-- 1.uzdevums
+-- use sql_hr;
+-- select * from employees;
+
+-- 2.uzdevums
+-- use sql_store;
+-- select first_name from customers;
+
+-- 3.uzdevums
+-- use sql_hr;
+-- select * from employees where reports_to is null;
+
+-- 4.uzdevums
+use sql_store;
+select * from orders where status = 1;
+
