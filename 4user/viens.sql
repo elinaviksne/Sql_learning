5fe897df17ca31c863ebbbe053741564456ce41c
Elina <elinaviksne124@gmail.com>
2024-10-28 10:12:11 +0200

    Create user and grant all privilages

--- 4user/viens.sql
+++ 4user/viens.sql
@@ -3,3 +3,5 @@ select * from mysql.user;
 create user php_app@localhost identified by "1234";
 -- drop user php_app@localhost;
 
+grant all privileges on  sql_hr.* to php_app@localhost;
+flush privileges;
